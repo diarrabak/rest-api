d4f2e9e35927c710861f91efd4c9f5956a635ccf
diarrabak <71572448+diarrabak@users.noreply.github.com>
2022-08-13 22:30:32 -0400

    Delete tokens

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -94,7 +94,7 @@ class UserController extends Controller
 
     public function logout()
     {
-        Auth()->user()->tokens()->delete;
+        Auth()->user()->tokens()->delete();
         return [
             'message' => 'logout successfully',
         ];
